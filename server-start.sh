2e43deb3aa164f55481191a9c45dd126db5d4ddc
Marvin-VW <82050751+Marvin-VW@users.noreply.github.com>
2024-02-15 08:29:06 +0100

    update server-start.sh

--- server-start.sh
+++ server-start.sh
@@ -9,14 +9,12 @@ run_serve() {
 
 start_node_server() {
     echo "Starting Node.js server..."
-    # Zum aktuellen Pfad wechseln und dann in das Verzeichnis wechseln
     cd "$CURRENT_PATH"/KARL-SERVER/
     gnome-terminal --title="Node.js Server" -- bash -c "node server.js; exec bash"
 }
 
 run_python_script() {
     echo "Running Python script..."
-    # Zum aktuellen Pfad wechseln und dann in das Verzeichnis wechseln
     cd "$CURRENT_PATH"/KARL-SERVER/public/python/
     gnome-terminal --title="Python Script" -- bash -c "python3 python.py; exec bash"
 }
